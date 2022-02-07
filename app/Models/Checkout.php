80cfce97e38c70424bb5c35426cedee4bf6e3b5f
anzanulzikra <zikra.zigzag1@gmail.com>
2022-02-07 10:43:54 +0800

    finish migration video 7

---
+++ app/Models/Checkout.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Checkout extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'camp_id',
+        'payment_status',
+        'midtrans_url',
+        'midtrans_booking_code'
+    ];
+}
