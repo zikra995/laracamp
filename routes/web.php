d4d68921aba648c3c37fa9b927888f538bef7e56
anzanulzikra <zikra.zigzag1@gmail.com>
2022-02-11 19:20:39 +0800

    slicing checout succes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('/sucess-checkout', function () {
+    return view('sucess_checkout');
+})->name('sucess-checkout');
